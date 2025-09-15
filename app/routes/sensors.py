b4da9f6ccde492a9e8420a341ef03efa2ada5995
Alfredo Santos <thirdsantos666@gmail.com>
2025-09-15 13:50:47 +0800

    changed url param into int

--- app/routes/sensors.py
+++ app/routes/sensors.py
@@ -4,7 +4,7 @@ from app.services import db, save_sensors, initialize_data_firebase, save_hourly
 
 sensors_bp = Blueprint("sensor", __name__)
 
-@sensors_bp.route("/<aquarium_id>/sensors", methods = ["POST"])
+@sensors_bp.route("/<int:aquarium_id>/sensors", methods = ["POST"])
 def sensors(aquarium_id):
   data = request.json
   initialize_data_firebase(aquarium_id)
@@ -14,7 +14,7 @@ def sensors(aquarium_id):
   return jsonify({"Message" : "Successfully recieved",
                   "Data" : data}), 200
 
-@sensors_bp.route("/<aquarium_id>/hourly_log", methods = ["POST"])
+@sensors_bp.route("/<int:aquarium_id>/hourly_log", methods = ["POST"])
 def hourly_log(aquarium_id):
   data = request.json
   initialize_data_firebase(aquarium_id)
